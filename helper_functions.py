37f60265d34e06cd5dd77f63b43209036ac65924
SaulGonzalez81 <sgonz081@ucr.edu>
2021-02-13 15:12:48 -0800

    Currently solves puzzles up to depth 12 within a reasonable amount of time

--- helper_functions.py
+++ helper_functions.py
@@ -14,12 +14,20 @@ def prog_input():
         val = int(input("Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle: "))
     if val == 2:
         print("Enter your puzzle, use a zero to represent the blank\n")
+        
         userList1 = input("Enter the first row, use space or tabs between numbers: ")
+        print(type(userList1[0]))
+        userList1 = list(map(int,userList1))
         print("\n")
+
         userList2 = input("Enter the second row, use space or tabs between numbers: ")
+        userList2 = list(map(int,userList2))
         print("\n")
+        
         userList3 = input("Enter the third row, use space or tabs between numbers: ")
+        userList3 = list(map(int,userList3))
         print("\n")
+        
         return [[userList1],[userList2],[userList3]]
     elif val == 1:
-        return [['1','2','3'],['4','8','0'],['7','6','5']]
+        return [[1,3,6],[5,0,7],[4,8,2]]
--- solver.py
+++ solver.py
@@ -1,5 +1,4 @@
 import copy
-from collections import deque
 from node import Node, fN_getter
 from helper_functions import index_loc, prog_input 
 
@@ -43,14 +42,17 @@ def f_n(node,option):
 def queuing_function(nodes,childNodes,queuingFunctionOption):
     for i,child in enumerate(childNodes):
         child.fN = f_n(copy.deepcopy(child),queuingFunctionOption)
-    childNodes = sorted(childNodes,key=fN_getter)
-    return childNodes
+    if queuingFunctionOption != 1 :
+        childNodes = sorted(childNodes,key=fN_getter)
+    for i,val in enumerate(childNodes):
+        nodes.append(val)    
+    return nodes
 
 
 #We create child nodes from our given node based on the legal operators that we can use to change the board state
 def expand(node):
     #(len(node.problem[0]))
-    childNodes = deque()
+    childNodes = []
     blankSpaceLoc = index_loc(0,node.problem) 
 
     #Check if moving the blank tile up works
@@ -59,45 +61,58 @@ def expand(node):
         tmp = cnode1.problem[blankSpaceLoc[0]][blankSpaceLoc[1]]
         cnode1.problem[blankSpaceLoc[0]][blankSpaceLoc[1]] = cnode1.problem[blankSpaceLoc[0]-1][blankSpaceLoc[1]]
         cnode1.problem[blankSpaceLoc[0]-1][blankSpaceLoc[1]] = tmp
+        cnode1.gN = cnode1.gN + 1
         childNodes.append(cnode1)        
 
     #Check if moving the blank tile down works
     if (blankSpaceLoc[0] + 1) < (len(node.problem[0])):
         cnode2 = copy.deepcopy(node)
         tmp = cnode2.problem[blankSpaceLoc[0]][blankSpaceLoc[1]]
-        cnode2.problem[blankSpaceLoc[0]][blankSpaceLoc[1]] = cnode2.problemp[blankSpaceLoc[0]+1][blankSpaceLoc[1]]
+        cnode2.problem[blankSpaceLoc[0]][blankSpaceLoc[1]] = cnode2.problem[blankSpaceLoc[0]+1][blankSpaceLoc[1]]
         cnode2.problem[blankSpaceLoc[0]+1][blankSpaceLoc[1]] = tmp
+        cnode2.gN = cnode2.gN + 1
         childNodes.append(cnode2)
     
     #Check if moving the blank tile left works
     if 0 <= (blankSpaceLoc[1] - 1):
-
+        cnode3 = copy.deepcopy(node)
+        tmp = cnode3.problem[blankSpaceLoc[0]][blankSpaceLoc[1]]
+        cnode3.problem[blankSpaceLoc[0]][blankSpaceLoc[1]] = cnode3.problem[blankSpaceLoc[0]][blankSpaceLoc[1]-1]
+        cnode3.problem[blankSpaceLoc[0]][blankSpaceLoc[1]-1] = tmp
+        cnode3.gN = cnode3.gN + 1
+        childNodes.append(cnode3)
     
     #Check if moving the blank tile right works
     if (blankSpaceLoc[1] + 1) < (len(node.problem[0])):
+        cnode4 = copy.deepcopy(node)
+        tmp = cnode4.problem[blankSpaceLoc[0]][blankSpaceLoc[1]]
+        cnode4.problem[blankSpaceLoc[0]][blankSpaceLoc[1]] = cnode4.problem[blankSpaceLoc[0]][blankSpaceLoc[1]+1]
+        cnode4.problem[blankSpaceLoc[0]][blankSpaceLoc[1]+1] = tmp
+        cnode4.gN = cnode4.gN + 1
+        childNodes.append(cnode4)
     return childNodes
 
 
 #This is where we will perform our search using the algorithm that the user desires 
 def general_search(problem,queuingFunctionOption):
     
-    #We first create a queue to hold our nodes. I chose a deque rather than a queue because it is premade and does everything a queue can do.
-    nodes = deque()
+    #We first create a queue to hold our nodes
+    nodes = []
     nodes.append(Node(problem,0,0))
     
-    #We want to continue this while loop until the length of the deque is equal to zero 
+    #We want to continue this while loop until the length of the queue is equal to zero 
     while len(nodes) > 0:
-        #We are looking at the front of the deque by popping left
-        node = nodes.popleft()
-        
-        #If the node at the front of the deque is the goal state, then we are finished and we can leave
-        if node == goalState:
+        #We are looking at the front of the queue by popping left
+        node = nodes.pop(0)
+
+        #If the node at the front of the queue is the goal state, then we are finished and we can leave
+        if node.problem == goalState:
             return node
         
         #We didn't reach the goal state, so now we call on the queuing_fucntion to push the next possible states into the queue
         nodes = queuing_function(nodes,expand(node),queuingFunctionOption)
     
-    #If we go through the entire deque without reaching the goal state, we can call this a failure
+    #If we go through the entire queue without reaching the goal state, we can call this a failure
     return "failure"
 
 
@@ -105,13 +120,17 @@ def general_search(problem,queuingFunctionOption):
 def main_function():
     #We use a function to grab the input from the user and return in a usuable format
     problem = prog_input()
-    
+
     #Now we are recieving the user's choice of alogirthm and that will determine what kind of heuristic we will use in our general search
     option = int(input("Enter your choice of algorithm\n1. Uniform Cost Search.\n2. A* with the Misplaced Tile heuristic.\n3. A* with the Manhattan distance heuristic.\n"))
     while option != 1 and option != 2 and option != 3:
         option = int(input("Enter your choice of alogirthm\n1. Uniform Cost Search.\n2. A* with the Misplaced Tile heuristic.\n3. A* with the Manhattan distance heuristic.\n"))
     
     #This is a little different from the psuedocode in the book, however I find this way to be a little easier to follow given our input structure.
-    general_search(problem,option)
-
+    result = general_search(problem,option)
+    print('\n')
+    if result != "failure":
+        print("Depth of Solution: ", result.gN)
+        for i,row in enumerate(result.problem):
+            print(*row)
 main_function()
